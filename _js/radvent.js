5af395a9bad3c999716de73686fa104167de5b3e
mrdestructicity <34351356+mrdestructicity@users.noreply.github.com>
2019-11-14 14:44:48 -0500

    Create radvent.js

---
+++ _js/radvent.js
@@ -0,0 +1,54 @@
+var SCHEDULE;
+
+var date = new Date;
+var request = new XMLHttpRequest;
+
+var today = date.getDate() - 1;
+var doors = document.querySelectorAll( '#doors button' );
+var track = document.querySelector( '#player' );
+var trackInfo = document.querySelector( '#player p');
+var trackAudio = document.querySelector( '#player audio');
+
+request.open('GET', '_js/radvent.json', true);
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400){
+		SCHEDULE = JSON.parse(request.responseText);
+		doors.forEach( function( element, index ) {
+
+			if ( index <= today ) {
+				if ( index === today ) {
+					element.classList.add( 'today' );
+				} else {
+					element.classList.add( 'ajar' );
+				}
+				element.title = `${SCHEDULE[index].song} by ${SCHEDULE[index].artist}`;
+				element.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="${SCHEDULE[index].song} by ${SCHEDULE[index].artist}" />`;
+				element.addEventListener( 'click', function( event ) {
+					event.preventDefault();
+					if ( this.classList.contains( 'open' ) ) {
+						track.classList.add( 'open' );
+						trackInfo.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="" /><strong>${SCHEDULE[index].song}</strong> by ${SCHEDULE[index].artist}`;
+						trackAudio.innerHTML = `<source src="${SCHEDULE[index].audio}" type="audio/mpeg" />`;
+						trackAudio.play();
+					} else {
+						this.classList.add( 'open' );
+					}
+					return false;
+				});
+
+			} else {
+				element.disabled = true;
+				element.addEventListener('click', function( event ) {
+					event.preventDefault();
+					console.log('No skipping ahead, cheater!');
+					return false;
+				});
+
+			}
+
+		});
+
+	}
+};
+
+request.send();
