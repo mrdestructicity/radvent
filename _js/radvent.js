228be158d6ed2f405a3f785d516691f86525284f
mrdestructicity <34351356+mrdestructicity@users.noreply.github.com>
2019-11-23 20:29:45 -0500

    JavaScript improvements
    Force audio element to reload on click

--- _js/radvent.js
+++ _js/radvent.js
@@ -11,44 +11,46 @@ var trackAudio = document.querySelector( '#player audio');
 
 request.open('GET', '_js/radvent.json', true);
 request.onload = function() {
-	if (request.status >= 200 && request.status < 400){
-		SCHEDULE = JSON.parse(request.responseText);
-		doors.forEach( function( element, index ) {
-
-			if ( index <= today ) {
-				if ( index === today ) {
-					element.classList.add( 'today' );
-				} else {
-					element.classList.add( 'ajar' );
-				}
-				element.title = `${SCHEDULE[index].song} by ${SCHEDULE[index].artist}`;
-				element.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="${SCHEDULE[index].song} by ${SCHEDULE[index].artist}" />`;
-				element.addEventListener( 'click', function( event ) {
-					event.preventDefault();
-					if ( this.classList.contains( 'open' ) ) {
-						track.classList.add( 'open' );
-						trackInfo.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="" /><strong>${SCHEDULE[index].song}</strong> by ${SCHEDULE[index].artist}`;
-						trackAudio.innerHTML = `<source src="${SCHEDULE[index].audio}" type="audio/mpeg" />`;
-						trackAudio.play();
-					} else {
-						this.classList.add( 'open' );
-					}
-					return false;
-				});
-
-			} else {
-				element.disabled = true;
-				element.addEventListener('click', function( event ) {
-					event.preventDefault();
-					console.log('No skipping ahead, cheater!');
-					return false;
-				});
-
-			}
-
+    if (request.status >= 200 && request.status < 400){
+	SCHEDULE = JSON.parse(request.responseText);
+	doors.forEach( function( element, index ) {
+	    
+	    if ( index <= today ) {
+		if ( index === today ) {
+		    element.classList.add( 'today' );
+		} else {
+		    element.classList.add( 'ajar' );
+		}
+		element.title = `${SCHEDULE[index].song} by ${SCHEDULE[index].artist}`;
+		element.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="${SCHEDULE[index].song} by ${SCHEDULE[index].artist}" />`;
+		element.addEventListener( 'click', function( event ) {
+		    event.preventDefault();
+		    if ( this.classList.contains( 'open' ) ) {
+			track.classList.add( 'open' );			
+			trackInfo.innerHTML = `<img src="${SCHEDULE[index].artwork}" alt="${SCHEDULE[index].song} by ${SCHEDULE[index].artist}" /><strong>${SCHEDULE[index].song}</strong> by ${SCHEDULE[index].artist}`;
+			trackAudio.pause();
+			trackAudio.innerHTML = `<source src="${SCHEDULE[index].audio}" type="audio/mpeg" />`;
+			trackAudio.load();
+			trackAudio.play();
+		    } else {
+			this.classList.add( 'open' );
+		    }
+		    return false;
 		});
-
-	}
+		
+	    } else {
+		element.disabled = true;
+		element.addEventListener('click', function( event ) {
+		    event.preventDefault();
+		    console.log('No skipping ahead, cheater!');
+		    return false;
+		});
+		
+	    }
+	    
+	});
+	
+    }
 };
 
 request.send();
